8486a0df4100c92d7b053fb6b211aa88589ae375
Raghuvar arora <raghuvararora@gmail.com>
2022-09-17 20:13:39 -0400

    corrected the sh file

--- desig-inits.sh
+++ desig-inits.sh
@@ -8,21 +8,20 @@ PROJECT_HOME="./"
 PATH_TO_MAIN_FILE="$PROJECT_HOME/parser/src"
 PATH_TO_CLASS_FILE="$PROJECT_HOME/parser/bin"
 #On the VM - i571b -> /home/apal5/projects/i571b
-
 read INPUT;
-echo "Input Read $INPUT";
+# echo "Input Read $INPUT";
  
 
 #Compilation
-echo "-------------Compilation-------------"
+# echo "-------------Compilation-------------"
 cd ${PATH_TO_MAIN_FILE}
-echo "javac ${PATH_TO_MAIN_FILE}/${MAIN_FILE} -d ${PATH_TO_CLASS_FILE}"
+# echo "javac ${PATH_TO_MAIN_FILE}/${MAIN_FILE} -d ${PATH_TO_CLASS_FILE}"
 javac ${MAIN_FILE} -d ${PATH_TO_CLASS_FILE}
 
 cd ${dir}
 
 #Run
 cd ${PATH_TO_CLASS_FILE}
-echo "-------------Execution-------------"
+# echo "-------------Execution-------------"
 java ${MAIN_CLASS} $INPUT;
 cd ${dir}
